85e4fc4f944915d9146a030432c7ecc5075f7224
Jonas Hörberg <jonas@tinkerton.se>
2016-09-15 13:23:11 +0200

    google search

--- src/public/client.js
+++ src/public/client.js
@@ -2,11 +2,19 @@
 class SearchBox extends React.Component {
   constructor() {
     super();
+    this.state = {
+        inputfield: ''
+      };
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(event) {
+    this.setState({inputfield: event.target.value});
+  }
+
   handleClick() {
-    console.log("clicked");
+    console.log("clicked " +  this.state.inputfield);
   /*  var arr = [];
     for (var i=0, t=20; i<t; i++) {
         arr.push(Math.round(Math.random() * t))
@@ -18,12 +26,13 @@ class SearchBox extends React.Component {
       document.getElementById('searchresult')
     );
 */
-$.get('http://localhost:3000/searchGoogle?searchText=restaurant+manhattan')
+$.getJSON('http://localhost:3000/searchGoogle?searchText=restaurant+'+  this.state.inputfield)
   .done(function(result) {
-    console.log(result);
+    var obj = JSON.parse(result.text);
+        console.log(obj);
     ReactDOM.render(
 
-      <ProperListRender list={result.jobs}  />,
+      <ProperListRender list={obj.results}  />,
       document.getElementById('searchresult')
     );
 });
@@ -35,7 +44,7 @@ $.get('http://localhost:3000/searchGoogle?searchText=restaurant+manhattan')
         <div className="welcomeText">Enter your location:</div>
         <div className="searchWrapper">
           <form>
-            <input type="text" placeholder="Enter location" />
+            <input type="text" placeholder="Enter location" onChange={this.handleChange} />
           <div className="submitButton" onClick={this.handleClick}>SEARCH</div>
           </form>
           </div>
@@ -49,8 +58,8 @@ class ProperListRender extends React.Component {
    render() {
      return (
        <ul>
-         {this.props.list.map(function(job, key){
-           return <li key={key}>{job.company_name}</li>;
+         {this.props.list.map(function(restaurant, key){
+           return <li key={key}>{restaurant.name}</li>;
          })}
        </ul>
      );
